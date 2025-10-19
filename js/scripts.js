5034719a79d53168580821224838c2683f700240
imkardo <kardoheydari.1387@gmail.com>
2025-10-19 18:09:16 +0330

    Nav bar - JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 const toggler = document.querySelector(".nav__toggler");
 const navbar = document.querySelector(".nav");
 toggler.addEventListener("click",(e)=>{
+        console.log("CLICKED")
         navbar.classList.toggle("nav__expanded")
 })
